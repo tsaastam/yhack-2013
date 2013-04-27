7ff4b1b597a3c4f0c5d72e68396f33c7ef80a448
mseri <marcello.seri@gmail.com>
2013-04-27 17:35:13 +0100

    restructured files

---
+++ expedia.js
@@ -0,0 +1,36 @@
+YUI().use('jsonp', 'jsonp-url', function (Y) {
+  function getExpediaUrl(city, ccode) {
+    var url =
+    "http://api.ean.com/ean-services/rs/hotel/v3/list?minorRev=22"+
+    "&cid=55505"+
+    "&apiKey=<INSERT_API_KEY_HERE>"+
+    "&locale=en_US"+
+    "&currencyCode=GBP"+
+    "&city="+city+
+    "&countryCode="+ccode+
+    "&supplierCacheTolerance=MED_ENHANCED"+
+    "&arrivalDate=04/27/2013"+
+    "&departureDate=04/31/2013"+
+    "&room1=2"+
+    "&numberOfResults=10"+
+    "&callback={callback}";
+    return url
+    }
+  
+  function getHotelInfo(resp) {
+    console.log(resp);
+    for(property in resp) {
+      console.log(property)
+    }
+    console.log(resp["HotelListResponse"])
+    console.log(resp["HotelListResponse"]["HotelList"])
+    console.log(resp["HotelListResponse"]["HotelList"]["HotelSummary"])
+    console.log(resp["HotelListResponse"]["HotelList"]["HotelSummary"][0])
+    console.log(resp["HotelListResponse"]["HotelList"]["HotelSummary"][0]["address1"])
+    var firstHotelAddr = resp["HotelListResponse"]["HotelList"]["HotelSummary"][0]["address1"];
+    document.getElementById("result").innerHTML = "first hotel address: "+firstHotelAddr
+  }
+
+  Y.jsonp(getExpediaUrl(city, ccode), getHotelInfo);
+
+});

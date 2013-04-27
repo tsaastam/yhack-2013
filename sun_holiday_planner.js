f307e4beffda32d47c3d3e35e46409decda187a4
mseri <marcello.seri@gmail.com>
2013-04-27 17:12:55 +0100

    improved js structure and data collection

--- sun_holiday_planner.js
+++ sun_holiday_planner.js
@@ -1,55 +1,68 @@
 var map;
-                var geocoder;
-                var weatherLayer;
-                
-                //var myLocation = "London, UK";
-                
-                function initialize() {
-                    
-                    var mapOptions = {
-                        zoom: 6,
-                        center: new google.maps.LatLng(51.511,-0.118),
-                        mapTypeId: google.maps.MapTypeId.ROADMAP,
-                    };
-                    
-                    map = new google.maps.Map(document.getElementById('map-canvas'),
-                                                  mapOptions);
-                    
-                    weatherLayer = new google.maps.weather.WeatherLayer({
-                                                                            temperatureUnits: google.maps.weather.TemperatureUnit.CELSIUS,
-                                                                            suppressInfoWindows: true
-                                                                            });
-                    
-                    weatherLayer.setMap(map);
+var geocoder;
+var weatherLayer;
 
-                    google.maps.event.addListener(weatherLayer, "click", function(smallPopup){
-                      var meteoForecast = document.getElementById('meteo-forecast');
-                      meteoForecast.innerHTML = smallPopup.infoWindowHtml;
+// Add trim support to older browsers
+if(typeof(String.prototype.trim) === "undefined")
+{
+    String.prototype.trim = function() 
+    {
+        return String(this).replace(/^\s+|\s+$/g, '');
+    };
+}
 
-                      alert(smallPopup.featureDetails.location);
-                    });
-                    
-                    /*var cloudLayer = new google.maps.weather.CloudLayer();
-                     cloudLayer.setMap(map);*/
-                    
-                    geocoder = new google.maps.Geocoder();
-                }
-                
-                function codeAddress() {
-                  var address = document.getElementById('address').value;
+function initialize() {
 
-                  geocoder.geocode( { 'address': address}, function(results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                      map.setCenter(results[0].geometry.location);
-                      map.setZoom(9);  // I like 9-10 but play around and see what you like
-                      var marker = new google.maps.Marker({
-                          map: map,
-                          position: results[0].geometry.location
-                      });
-                    } else {
-                      alert('Geocode was not successful for the following reason: ' + status);
-                    }
-                  });
-                }
+  var mapOptions = {
+    zoom: 6,
+    center: new google.maps.LatLng(51.511,-0.118),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  };
 
-                google.maps.event.addDomListener(window, 'load', initialize);
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  weatherLayer = new google.maps.weather.WeatherLayer({
+    temperatureUnits: google.maps.weather.TemperatureUnit.CELSIUS,
+    suppressInfoWindows: true
+  });
+
+  weatherLayer.setMap(map);
+
+  google.maps.event.addListener(weatherLayer, "click", function(smallPopup){
+    manageWeatherClick(smallPopup);
+  });
+
+    /*var cloudLayer = new google.maps.weather.CloudLayer();
+    cloudLayer.setMap(map);*/
+    
+    geocoder = new google.maps.Geocoder();
+}
+
+function codeAddress() {
+  var address = document.getElementById('address').value;
+
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        map.setZoom(9);  // I like 9-10 but play around and see what you like
+        var marker = new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+function manageWeatherClick(smallPopup) {
+  var meteoForecast = document.getElementById('meteo-forecast');
+  meteoForecast.innerHTML = smallPopup.infoWindowHtml;
+
+  var locationName = smallPopup.featureDetails.location.split(',')[0].trim();
+  var countryName = smallPopup.featureDetails.location.split(',')[1].trim();
+  alert(locationName + " -> " + countryName);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

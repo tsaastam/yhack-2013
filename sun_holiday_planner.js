49f4f96fd56b61f3155d117e1c5b8dd4b31a0a3a
mseri <marcello.seri@gmail.com>
2013-04-28 12:36:08 +0100

    solved small bug not clearing flickr on new search

--- sun_holiday_planner.js
+++ sun_holiday_planner.js
@@ -87,13 +87,15 @@ function codeAddress() {
     var flickrH = document.getElementById('flickrH');
     
     
-    document.getElementById('textWE').style.visibility='visible';
-    document.getElementById('textHO').style.visibility='visible';
-    document.getElementById('textFL').style.visibility='visible';
+    // getElementById not working with h1 on some browsers...
+    //document.getElementById('textWE').style.visibility='visible';
+    //document.getElementById('textHO').style.visibility='visible';
+    //document.getElementById('textFL').style.visibility='visible';
     weatherH.innerHTML = " ";
     hotelsH.innerHTML = " ";
     flickrH.innerHTML = " ";
     document.getElementById('resultF').innerHTML = " ";
+    //console.log(document.getElementById('resultF'));
 
     window.scrollTo(-100, -100);
   });

7f466ca60403efee50b2b12c2df2cb66abcf229d
mseri <marcello.seri@gmail.com>
2013-04-28 10:58:36 +0100

    Nicer flickr grid

--- expedia.js
+++ expedia.js
@@ -60,7 +60,7 @@ YUI().use('jsonp', 'jsonp-url', 'yql', 'yql-jsonp', function (Y) {
 
     for(var i =0; i<maxIter; i++ ) { 
       //info += '    <div class="flickrContainer" style="background-image: url(\'' + pics[i].url_t.replace('_t','_q') +'\'); height: 200px; width: 200px; background-repeat:no-repeat; background-position:center center; background-size: 150px 150px; overlay: hidden; float:left;"> </div>​\n';
-      info += '    <div style="height: 200px; width: 200px; overlay: hidden; float:left;"><img src="' +  pics[i].url_t.replace('_t','_q') + '" style="width:150px;"></img> </div>​\n';
+      info += '    <div style="height: 180px; width: 180px; overlay: hidden; float:left;"><img src="' +  pics[i].url_t.replace('_t','_q') + '" style="width:150px;"></img> </div>​\n';
       // for the actual url is enough: pics[i].url_t.replace("_t","")
       //info += "    <img  width=\"110px\" src='" + pics[i].url_t + "'/>";
       // for the actual url is enough: pics[i].url_t.replace("_t","")
